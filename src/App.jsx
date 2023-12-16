b04e6387e17f66ce986ff121476a9c86ee2c73d3
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-12-16 17:04:17 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,52 @@
-import './App.css';
-import { useState } from 'react';
+import { Component } from "react";
+import "./App.css";
 
-function App({display}) {
-  const[num, setNum] = useState(0);
-  
-  const incNum = () =>{
-    setNum ( num +1)
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num: 0,
+    };
+  }
+
+  incNum = () => {
+    this.setState((prevState) => ({ num: prevState.num + 1 }));
   };
 
-  const decNum =() =>{
-    if (num > 0){
-    setNum(num-1)
-  }else{
-    setNum(0)
-  }
+  decNum = () => {
+    this.setState((prevState) => ({
+      num: prevState.num > 0 ? prevState.num - 1 : 0,
+    }));
   };
-  const reset=() =>{
-    setNum(0)
+
+  reset = () => {
+    this.setState({ num: 0 });
+  };
+
+  render() {
+    const { num } = this.state;
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="container">
+            <h1>Counter-App</h1>
+            <h2>{num}</h2>
+
+            <button onClick={this.incNum} className="inc">
+              +
+            </button>
+            <button onClick={this.decNum} className="dec">
+              -
+            </button>
+            <button onClick={this.reset} className="reset">
+              Reset
+            </button>
+          </div>
+        </header>
+      </div>
+    );
   }
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className='container'>
-          <h1>Counter-App</h1>
-          <h2>{num}</h2>
-
-          <button onClick={incNum} className='inc'>
-            +
-          </button>
-          <button onClick={decNum} className='dec'>
-            -
-          </button>
-          <button onClick={reset} className='reset'>
-            Reset
-          </button>
-     
-
-        </div>
-      </header>
-    </div>
-  );
 }
 
-export default App;
+
